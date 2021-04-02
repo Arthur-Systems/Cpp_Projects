fc87191864d39b397592ac0e2b851574bbe79129
Haichuan Wei <arthur.wei50@gmail.com>
2021-04-02 12:21:53 -0700

    5

--- Lab 6.1/Source.cpp
+++ Lab 6.1/Source.cpp
@@ -2,23 +2,26 @@
 #include<cmath>
 using namespace std;
 
-int patternFunction() {
-	for (int row = 0; row <= maxRows; row++) {
-
+int patternFunction(int maxRows) {
+	if (maxRows < 0) {
+		cout << "Wrong max row number: " << maxRows << endl;
+		return false;
+	}
+	for (int row = 0; row < maxRows; row++) {
 		for (int col = 0; col <= row; col++) {
-			cout << col;
+			int num = col+1;
+			cout << num;
 		}
-
 		cout << endl;
 
 	}
-
+	return true;
 };
 
 int main() {
 	bool pattern = false;
 	int maxRows = 0;
-	
+	cin >> maxRows;
 	pattern = patternFunction(maxRows);
 
 	if (pattern) cout << "Fuction Worked";
