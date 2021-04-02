fbb26a552fbb169dc8c7fe00d4aa26f875ecbc8c
Arthur-Systems <arthur.wei50@gmail.com>
2021-04-02 10:56:18 -0700

    123

---
+++ Lab 6.1/Source.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	int maxRows = 5;
+	bool pattern = false;
+	pattern = patternFunction(maxRows);
+
+	if (pattern) cout << "Fuction Worked";
+		return 0;
+	//end of main function
+
+
+}
---
+++ Lab 6/Source.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int maxRows = 0;
+	cout << "how many Rows?";
+	cin >> maxRows;
+	for (int row = 0; row <= maxRows; row ++) {
+		for (int col = 0; col <= row; col ++) {
+			cout << col;
+			//end for
+			//cout << endl;
+		} //end for
+		cout << endl;
+	}
+	return 0;
+	//end of main function
+
+
+}
