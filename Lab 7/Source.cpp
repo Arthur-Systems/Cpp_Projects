29ca753a8ad625948473571662918b6ef8f66b89
Arthur-Systems <arthur.wei50@gmail.com>
2021-04-07 12:37:20 -0700

    1

--- Lab 7/Source.cpp
+++ Lab 7/Source.cpp
@@ -3,16 +3,31 @@
 using namespace std;
 
 void multiFunction(int secoundnum, int firstnum) {
-	
-	for (int y = 0; y <= 11; y++) {
-
-				
-	}
+int total = secoundnum*firstnum;
+cout << firstnum<<"*"<<secoundnum<<"="<<total<<endl;
 };
+
 int main() {
-	int firstnum= 0;
-	int secoundnum = 0;
-	multiFunction(secoundnum, firstnum);
+	int firstnum = 1 , secoundnum = 0;
+while(firstnum <= 12){
 	
+for(int x = 0;x < 12; x++){
+	secoundnum++;
+	multiFunction(secoundnum, firstnum);
+	}
+	if(secoundnum >=12) {
+	cout<<endl;
+	secoundnum = 0;
+	firstnum++;
+	} 
 
 }
+	
+}
+	
+	
+
+	
+	
+	//cout<<"test" <<endl;
+
