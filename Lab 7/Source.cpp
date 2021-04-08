479af5d5e8863dc2f2b35c2389e317a0faa94fab
Arthur-Systems <arthur.wei50@gmail.com>
2021-04-07 17:46:56 -0700

    Poof

--- Lab 7/Source.cpp
+++ Lab 7/Source.cpp
@@ -2,32 +2,29 @@
 
 using namespace std;
 
-void multiFunction(int secoundnum, int firstnum) {
-int total = secoundnum*firstnum;
-cout << firstnum<<"*"<<secoundnum<<"="<<total<<endl;
+void multiFunction(int secoundnum, int firstnum)
+{
+	int total = secoundnum * firstnum;
+	cout << firstnum << "*" << secoundnum << "=" << total << endl;
 };
 
-int main() {
-	int firstnum = 1 , secoundnum = 0;
-while(firstnum <= 12){
-	
-for(int x = 0;x < 12; x++){
-	secoundnum++;
-	multiFunction(secoundnum, firstnum);
+int main()
+{
+	int firstnum = 1, secoundnum = 0;
+	while (firstnum <= 12)
+	{
+		for (int x = 0; x < 12; x++)
+		{
+			secoundnum++;
+			multiFunction(secoundnum, firstnum);
+		}
+		if (secoundnum >= 12)
+		{
+			cout << endl;
+			secoundnum = 0;
+			firstnum++;
+		}
 	}
-	if(secoundnum >=12) {
-	cout<<endl;
-	secoundnum = 0;
-	firstnum++;
-	} 
-
 }
-	
-}
-	
-	
 
-	
-	
-	//cout<<"test" <<endl;
-
+//cout<<"test" <<endl;
