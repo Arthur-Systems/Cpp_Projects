613f0087a67ab51800ba3b71521ce28e4c18101e
Haichuan Wei <arthur.wei50@gmail.com>
2021-09-10 18:39:26 -0700

    Lab 1 complete

---
+++ Personal/Nested Loops/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+
+    for (int i = 1; i <= 5;)
+    {
+        for (int x; x <= i;)
+        {
+            cout << x << endl;
+        }
+        i++
+    }
+    return 0;
+}
